4e92e9c79538f0887d81ffac89a3fd26c314070b
Gourab <gourabgaming111@gmail.com>
2024-03-14 00:32:42 +0530

    tested form input

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 # Flask file
 
 # Importing necessary libraries
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
+import pickle
+
+# Importing the model
+model = pickle.load(open('models/model.pkl', 'rb'))
 
 # Creating a Flask object
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/form', methods=['POST','GET'])
+def form():
+    if request.method == 'POST':
+        input = request.form.get('input')
+    return render_template('result.html', input=input)
+    
+
 
 
 
