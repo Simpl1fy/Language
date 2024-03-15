83c842ab6f34a6dff39ebcca5a19c987956d6ee1
Gourab <gourabgaming111@gmail.com>
2024-03-15 20:03:35 +0530

    added directories and files and code for logger and custom exception

--- src/components/input_converter.py
+++
@@ -1,9 +0,0 @@
-# Importing necessary libraries
-
-from sklearn.feature_extraction.text import CountVectorizer
-
-def bag_of_words(text):
-    cv = CountVectorizer()
-    text_vectorized = cv.fit_transform(text).toarray()
-    return text_vectorized
-
