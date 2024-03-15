b2c7ac8e907332b3dc5b34e6e15a3def2830522c
Gourab <gourabgaming111@gmail.com>
2024-03-15 20:35:10 +0530

    fixed logger and added data ingestion for tranformation and model training

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+from src.logger import logging
+from src.exception import CustomException
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path:str = os.path.join('artifacts', 'train.csv')
+    test_data_path:str = os.path.join('artifacts', 'test.csv')
+    raw_data_path:str = os.path.join('artifacts', 'data.csv')
+
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    
+    def initiate_data_ingestion(self):
+        logging.info("Initiating Data Ingestion")
+        try:
+            df = pd.read_csv('notebook/data/Language Detection.csv')
+            logging.info("Read the csv file as a dataframe")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            logging.info("Created a directory with the name artifacts")
+
+            df.to_csv(self.ingestion_config.raw_data_path, header=True, index=False)
+
+            logging.info("Train test split initiated")
+            train_data, test_data = train_test_split(df, test_size=0.2)
+
+            train_data.to_csv(self.ingestion_config.train_data_path, header=True, index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path, header=True, index=False)
+
+            logging.info("ingestion of data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+    
+# Checking if data ingestion works
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
--- src/logger.py
+++ src/logger.py
@@ -1,6 +1,6 @@
 import logging
 import os
-import datetime
+from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(), "logs", LOG_FILE)
@@ -11,5 +11,5 @@ LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.info
+    level=logging.INFO,
 )
