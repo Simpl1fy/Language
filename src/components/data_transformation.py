ef9e7b47bf2970da2951e940b4a1b5f0a83ba526
Simpl1fy <gourabdas07012004@gmail.com>
2024-03-26 00:32:35 +0530

    testing

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 from src.utils import Remove, Vectorizer, save_object
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
+from sklearn.compose import ColumnTransformer
 
 
 from dataclasses import dataclass
@@ -17,7 +18,6 @@ import pickle
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
-    target_processor_obj_file_path = os.path.join('artifacts', 'target-processor.pkl')
 
 
 class DataTransformation:
@@ -30,18 +30,22 @@ class DataTransformation:
        ''' 
 
        try:
-           
         input_cols = ['Text']
-        output_cols = ['Language']
 
         input_pipeline = Pipeline([
             ("remove", Remove()),
             ("vectorizer", Vectorizer())
         ])
 
+        preprocessor = ColumnTransformer(
+            [
+                ("input-pipeline", input_pipeline, input_cols)
+            ]
+        )
+
         le = LabelEncoder()
 
-        return (input_pipeline, le)
+        return (preprocessor, le)
 
        except Exception as e:
            raise CustomException(e, sys)
@@ -54,27 +58,30 @@ class DataTransformation:
             train_data = pd.read_csv(train_path)
             test_data = pd.read_csv(test_path)
 
+            target_column_name = "Language"
+
             logging.info("Read of training and testing data complete")
 
             logging.info("Obtaining preprocessing object for input and output")
 
-            input_processing, output_processing = self.get_data_transformer_object()
+            input_processing, label_encoder = self.get_data_transformer_object()
 
-            
+            print(train_data.head())
+            print(test_data.head())
 
-            input_feature_train_df = train_data.drop(columns=["Language"], axis=1)
-            target_feature_train_df = train_data["Language"]
+            input_feature_train_df = train_data.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_data[target_column_name]
 
-            input_feature_test_df = test_data.drop(columns=["Language"], axis=1)
-            target_feature_test_df = test_data["Language"]
+            input_feature_test_df = test_data.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_data[target_column_name]
 
             logging.info("Applying the processor objects in training and testing dataset")
 
             input_feature_train_arr = input_processing.fit_transform(input_feature_train_df)
             input_feature_test_arr = input_processing.transform(input_feature_test_df)
 
-            target_feature_train_arr = np.array(output_processing.fit_transform(target_feature_train_df))
-            target_feature_test_arr = np.array(output_processing.transform(target_feature_test_df))
+            target_feature_train_arr = np.array(label_encoder.fit_transform(target_feature_train_df))
+            target_feature_test_arr = np.array(label_encoder.transform(target_feature_test_df))
 
             train_arr = np.c_[input_feature_train_arr, target_feature_train_arr]
             test_arr = np.c_[input_feature_test_arr, target_feature_test_arr]
@@ -83,11 +90,6 @@ class DataTransformation:
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=input_processing
             )             
-
-            save_object(
-                file_path=self.data_transformation_config.target_processor_obj_file_path,
-                obj = output_processing
-            )   
 
             return (
                 train_arr,
